af4188771fa7bffe0ab6b2ce3f71c2330da2e3d7
Michael Maltese <mike@mikemaltese.com>
2013-09-01 18:45:33 -0700

    Update rbenv, ruby-build, and Ruby

--- build.sh
+++ build.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
 git clone git://github.com/sstephenson/rbenv.git rbenv
-( cd rbenv && git checkout tags/v0.3.0 )
+( cd rbenv && git checkout tags/v0.4.0 )
 
 git clone git://github.com/sstephenson/ruby-build.git rbenv/plugins/ruby-build
-( cd rbenv/plugins/ruby-build && git checkout tags/v20121204 )
+( cd rbenv/plugins/ruby-build && git checkout tags/v20130901 )
 
 export RBENV_ROOT="$(pwd)/rbenv"
 export PATH="$RBENV_ROOT/bin:$PATH"
-test -z "$(rbenv versions | grep 1.8.7-p370)" && rbenv install 1.8.7-p370
+export RUBY_VERSION="1.9.3-p448"
+test -z "$(rbenv versions | grep $RUBY_VERSION)" && rbenv install $RUBY_VERSION
 eval "$(rbenv init -)"
-rbenv shell 1.8.7-p370
+rbenv shell $RUBY_VERSION
 
 rbenv exec gem install bundler --no-ri --no-rdoc
 ( cd lib && rbenv exec bundle )
--- run.sh
+++ run.sh
@@ -2,7 +2,8 @@
 
 export RBENV_ROOT="$(pwd)/rbenv"
 export PATH="$RBENV_ROOT/bin:$PATH"
+export RUBY_VERSION="1.9.3-p448"
 eval "$(rbenv init -)"
-rbenv shell 1.8.7-p370
+rbenv shell $RUBY_VERSION
 mkdir -p www
 ( cd lib && bundle exec ruby controller.rb > ../www/index.html )
